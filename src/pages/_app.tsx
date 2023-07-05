01cdcfb5ab97b692dcaa26345f8dfb4c5007488f
Carmilla <carmillam@icloud.com>
2023-07-05 16:03:55 +0200

    v1

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
+import Script from 'next/script';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <>
+      <Script
+        async
+        strategy="afterInteractive"
+        type="module"
+        src="https://unpkg.com/@google/model-viewer@^2.1.1/dist/model-viewer.min.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
